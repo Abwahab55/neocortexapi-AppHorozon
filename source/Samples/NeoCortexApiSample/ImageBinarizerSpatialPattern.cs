3f3406122947b38900a6ca9c2064d722bffb8a81
Nabila000 <nabila.kodeeo@gmail.com>
2025-02-28 23:34:02 +0100

    image binarizer modify

--- source/Samples/NeoCortexApiSample/ImageBinarizerSpatialPattern.cs
+++ source/Samples/NeoCortexApiSample/ImageBinarizerSpatialPattern.cs
@@ -16,7 +16,7 @@ namespace NeoCortexApiSample
         public string inputPrefix { get; private set; } = "";
 
         public void Run()
-        {   //STARTING EXPERIMENT OF IMAGE BINARIZATION
+        {   //EXPERIMENT OF IMAGE BINARIZATION
             Console.WriteLine($" Starting Experiment: {nameof(ImageBinarizerSpatialPattern)}");
 
             double minOctOverlapCycles = 1.0;
@@ -167,7 +167,7 @@ namespace NeoCortexApiSample
                 Console.WriteLine(" No images found for restructuring.");
                 return;
             }
-            // PUTTING IMAGE SIZE AS REQUIRED
+            //PUTTING IMAGE SIZE AS REQUIRED
             int imgSize = 28;
             int[] activeArray = new int[32 * 32];
 
