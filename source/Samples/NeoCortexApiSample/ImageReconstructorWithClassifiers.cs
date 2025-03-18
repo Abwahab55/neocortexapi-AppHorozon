a58b3342f47c5b452a3f0ab027d6cafa2953751d
Nabila000 <nabila.kodeeo@gmail.com>
2025-03-19 00:37:31 +0100

    image file rechecked

--- source/Samples/NeoCortexApiSample/ImageReconstructorWithClassifiers.cs
+++ source/Samples/NeoCortexApiSample/ImageReconstructorWithClassifiers.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 //False one for testing binarization
+//comment for testing
 namespace NeoCortexApiSample
 {
     //testing dates
